ec85ef5fb36247674ac2cd62dbdef60fb65cdf87
Arithmomaniac <email@avilevin.net>
2016-06-28 23:02:23 -0500

    Finish API change into new format

--- src/OverrideXml.cs
+++ src/OverrideXml.cs
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 Ivan Krivyakov, http://www.ikriv.com/
+ * And (C) 2014 Avi Levin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,10 +13,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Xml.Serialization;
 
-namespace Ikriv.Xml
+namespace Arithmomaniac.OverrideXml
 {
 
 
@@ -40,6 +40,8 @@ namespace Ikriv.Xml
         protected abstract string PropertyName { get; }
         protected XmlAttributes Attributes { get; } = new XmlAttributes();
         internal XmlAttributeOverride Compile() => new XmlAttributeOverride(Class, PropertyName, Attributes);
+        
+
     }
 
     public class OverrideRootXml<T> : OverrideXmlSpec
@@ -48,6 +50,39 @@ namespace Ikriv.Xml
         internal OverrideRootXml() : base(typeof(T)) { }
 
         protected override string PropertyName => null;
+
+
+        protected OverrideRootXml<T> Xmlns(bool value)
+        {
+
+            Attributes.Xmlns = value;
+            return this;
+        }
+
+        public OverrideRootXml<T> XmlRoot(string elementName) => Attr(new XmlRootAttribute(elementName));
+        public OverrideRootXml<T> Attr(XmlRootAttribute xmlRoot)
+        {
+
+            Attributes.XmlRoot = xmlRoot;
+            return this;
+        }
+
+        
+        public OverrideRootXml<T> XmlType(string typeName) => Attr(new XmlTypeAttribute(typeName));
+
+        public OverrideRootXml<T> Attr(XmlTypeAttribute attribute)
+        {
+
+            Attributes.XmlType = attribute;
+            return this;
+        }
+
+        public OverrideRootXml<T> XmlDefaultValue(object value)
+        {
+
+            Attributes.XmlDefaultValue = value;
+            return this;
+        }
     }
 
     public class OverrideMemberXml<T> : OverrideXmlSpec
@@ -57,19 +92,103 @@ namespace Ikriv.Xml
         internal OverrideMemberXml(Expression<Func<T, object>> propertyLambda) : base(typeof(T))
         {
             //http://stackoverflow.com/a/672212/
-            var propInfo = (propertyLambda.Body as MemberExpression)?.Member as PropertyInfo;
+            var propInfo = (propertyLambda.Body as MemberExpression)?.Member;
             if (    propInfo == null ||
                     (typeof(T) != propInfo.ReflectedType && !typeof(T).IsSubclassOf(propInfo.ReflectedType))
             )
             {
-                throw new ArgumentException("A property expression was not provided", nameof(propertyLambda));
+                throw new ArgumentException("A member expression was not provided", nameof(propertyLambda));
             }
 
             PropertyName = propInfo.Name;
         }
+
+        public OverrideMemberXml<T> XmlAnyAttribute()
+        {
+
+            Attributes.XmlAnyAttribute = new XmlAnyAttributeAttribute();
+            return this;
+        }
+
+
+        public OverrideMemberXml<T> XmlText()
+        {
+
+            Attributes.XmlText = new XmlTextAttribute();
+            return this;
+        }
+
+        public OverrideMemberXml<T> XmlIgnore(bool bIgnore = true)
+        {
+
+            Attributes.XmlIgnore = bIgnore;
+            return this;
+        }
+
+        public OverrideMemberXml<T> XmlArray() => Attr(new XmlArrayAttribute());
+
+
+        public OverrideMemberXml<T> XmlArray(string elementName) => Attr(new XmlArrayAttribute(elementName));
+
+
+        public OverrideMemberXml<T> Attr(XmlArrayAttribute attribute)
+        {
+
+            Attributes.XmlArray = attribute;
+            return this;
+        }
+
+        public OverrideMemberXml<T> XmlArrayItem() => Attr(new XmlArrayItemAttribute());
+
+        public OverrideMemberXml<T> XmlArrayItem(string elementName) => Attr(new XmlArrayItemAttribute(elementName));
+
+        public OverrideMemberXml<T> Attr(XmlArrayItemAttribute attribute)
+        {
+
+            Attributes.XmlArrayItems.Add(attribute);
+            return this;
+        }
+
+        public OverrideMemberXml<T> XmlAttribute() => Attr(new XmlAttributeAttribute());
+
+        public OverrideMemberXml<T> XmlAttribute(string name) => Attr(new XmlAttributeAttribute(name));
+
+        public OverrideMemberXml<T> Attr(XmlAttributeAttribute attribute)
+        {
+
+            Attributes.XmlAttribute = attribute;
+            return this;
+        }
+
+        public OverrideMemberXml<T> XmlElement() => Attr(new XmlElementAttribute());
+        
+
+        public OverrideMemberXml<T> XmlElement(string name) => Attr(new XmlElementAttribute(name));
+        
+
+        public OverrideMemberXml<T> Attr(XmlElementAttribute attribute)
+        {
+
+            Attributes.XmlElements.Add(attribute);
+            return this;
+        }
+
+        public OverrideMemberXml<T> XmlAnyElement() => Attr(new XmlAnyElementAttribute());
+
+        public OverrideMemberXml<T> XmlAnyElement(string name) => Attr(new XmlAnyElementAttribute(name));
+
+        public OverrideMemberXml<T> XmlAnyElement(string name, string ns) => Attr(new XmlAnyElementAttribute(name, ns));
+
+        public OverrideMemberXml<T> Attr(XmlAnyElementAttribute attribute)
+        {
+
+            Attributes.XmlAnyElements.Add(attribute);
+            return this;
+        }
+
     }
 
-    public class OverrideXml2
+    public class OverrideXml
     {
         private List<OverrideXmlSpec> _overrides = new List<OverrideXmlSpec>();
 
@@ -85,7 +204,7 @@ namespace Ikriv.Xml
             return ovride;
             
         }
-             
+        
         public XmlAttributeOverrides Commit()
         {
             var overrides = new XmlAttributeOverrides();
@@ -96,269 +215,5 @@ namespace Ikriv.Xml
             return overrides;
         }
     }
-
-
-
-    /// <summary>
-    /// Creates XmlAttributeOverrides instance using an easy-to-use fluent interface
-    /// </summary>
-    public class OverrideXml
-    {
-        private XmlAttributes _attributes;
-        
-
-        /// <summary>
-        /// Adds [XmlRoot(elementName)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlRoot(string elementName)
-        {
-            
-            _attributes.XmlRoot = new XmlRootAttribute(elementName);
-            return this;
-        }
-
-        /// <summary>
-        /// Adds specified instance of XmlRootAttribute for current type or member
-        /// </summary>
-        public OverrideXml Attr(XmlRootAttribute xmlRoot)
-        {
-            
-            _attributes.XmlRoot = xmlRoot;
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlAttribute] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlAttribute()
-        {
-            
-            _attributes.XmlAttribute = new XmlAttributeAttribute();
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlAttribute(name)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlAttribute(string name)
-        {
-            
-            _attributes.XmlAttribute = new XmlAttributeAttribute(name);
-            return this;
-        }
-
-        /// <summary>
-        /// Adds specified instance of XmlAttributeAttribute to current type or member
-        /// </summary>
-        public OverrideXml Attr(XmlAttributeAttribute attribute)
-        {
-            
-            _attributes.XmlAttribute = attribute;
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlElement] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlElement()
-        {
-            
-            _attributes.XmlElements.Add(new XmlElementAttribute());
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlElement(name)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlElement(string name)
-        {
-            
-            _attributes.XmlElements.Add(new XmlElementAttribute(name));
-            return this;
-        }
-
-        /// <summary>
-        /// Adds specified instance of XmlElementAttribute to current type or member
-        /// </summary>
-        public OverrideXml Attr(XmlElementAttribute attribute)
-        {
-            
-            _attributes.XmlElements.Add(attribute);
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlIgnore] attribute to current type or member
-        /// </summary>
-        /// <param name="bIgnore"></param>
-        public OverrideXml XmlIgnore(bool bIgnore=true)
-        {
-            
-            _attributes.XmlIgnore = bIgnore;
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlAnyAttribute] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlAnyAttribute()
-        {
-            
-            _attributes.XmlAnyAttribute = new XmlAnyAttributeAttribute();
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlAnyElement] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlAnyElement()
-        {
-            
-            _attributes.XmlAnyElements.Add(new XmlAnyElementAttribute());
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlAnyElement(name)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlAnyElement(string name)
-        {
-            
-            _attributes.XmlAnyElements.Add(new XmlAnyElementAttribute(name));
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlAnyElement(name,ns)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlAnyElement(string name, string ns)
-        {
-            
-            _attributes.XmlAnyElements.Add(new XmlAnyElementAttribute(name, ns));
-            return this;
-        }
-
-        /// <summary>
-        /// Adds specified instance of XmlAnyElementAttribute to current type or member
-        /// </summary>
-        public OverrideXml Attr(XmlAnyElementAttribute attribute)
-        {
-            
-            _attributes.XmlAnyElements.Add(attribute);
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlArray] attribute to current type or memeber
-        /// </summary>
-        public OverrideXml XmlArray()
-        {
-            
-            _attributes.XmlArray = new XmlArrayAttribute();
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlArray(elementName)] attribute to current type or memeber
-        /// </summary>
-        public OverrideXml XmlArray(string elementName)
-        {
-            
-            _attributes.XmlArray = new XmlArrayAttribute(elementName);
-            return this;
-        }
-
-        /// <summary>
-        /// Adds specified instance of XmlArrayAttribute to current type or memeber
-        /// </summary>
-        public OverrideXml Attr(XmlArrayAttribute attribute)
-        {
-            
-            _attributes.XmlArray = attribute;
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlArrayItem] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlArrayItem()
-        {
-            
-            _attributes.XmlArrayItems.Add(new XmlArrayItemAttribute());
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlArrayItem(elementName)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlArrayItem(string elementName)
-        {
-            
-            _attributes.XmlArrayItems.Add(new XmlArrayItemAttribute(elementName));
-            return this;
-        }
-
-        /// <summary>
-        /// Adds specified instance of XmlArrayItemAttribute to current type or member
-        /// </summary>
-        public OverrideXml Attr(XmlArrayItemAttribute attribute)
-        {
-            
-            _attributes.XmlArrayItems.Add(attribute);
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlDefault(value)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlDefaultValue(object value)
-        {
-            
-            _attributes.XmlDefaultValue = value;
-            return this;
-        }
-
-        /// <summary>
-        /// Applies or removes [XmlNamespaceDeclarations] attribute from current type or member
-        /// </summary>
-        public OverrideXml Xmlns(bool value)
-        {
-            
-            _attributes.Xmlns = value;
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlText] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlText()
-        {
-            
-            _attributes.XmlText = new XmlTextAttribute();
-            return this;
-        }
-
-        /// <summary>
-        /// Adds [XmlType(typeName)] attribute to current type or member
-        /// </summary>
-        public OverrideXml XmlType(string typeName)
-        {
-            
-            _attributes.XmlType = new XmlTypeAttribute(typeName);
-            return this;
-        }
-
-        /// <summary>
-        /// Adds specified instance of XmlTypeAttribute to current type or member
-        /// </summary>
-        public OverrideXml Attr(XmlTypeAttribute attribute)
-        {
-            
-            _attributes.XmlType = attribute;
-            return this;
-        }
-
-    }
-
 
 }
